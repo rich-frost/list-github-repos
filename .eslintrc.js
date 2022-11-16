45e626367f8e17f00aa8b4783adff3afcd0e6cc5
Richard Frost <richard.frost@global.ntt>
2022-11-16 16:13:00 +0000

    Added .eslintrc.js

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  env: {
+    browser: true,
+    es2021: true,
+    commonjs: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  overrides: [],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react', '@typescript-eslint'],
+  rules: {},
+};
